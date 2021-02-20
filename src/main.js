184f3b5dfd8ccb6267418540851df4e55d4068aa
Mark Maddocks <mark.emaddocks@gmail.com>
2021-02-20 18:53:16 +0000

    Added analytics

--- src/main.js
+++ src/main.js
@@ -1,13 +1,9 @@
-// AOS Scroll (placed here so it can be overidden)
-import AOS from "aos";
+import AOS from "aos"; // AOS Scroll (placed here so it can be overidden)
 import "aos/dist/aos.css";
-
-// VUE
 import Vue from "vue";
 import App from "./App.vue";
-
-// Normalise css
 import "normalize.css";
+import VueAnalytics from "vue-analytics";
 
 // Polyfill to enable smooth scroll
 import smoothscroll from "smoothscroll-polyfill";
@@ -19,19 +15,20 @@ Vue.use(VueTypedJs);
 
 // FontAwesome imports
 import { library } from "@fortawesome/fontawesome-svg-core";
-
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { far } from "@fortawesome/free-regular-svg-icons";
 import { fab } from "@fortawesome/free-brands-svg-icons";
-
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-
 // Adding all icons
 library.add(fas, far, fab);
-
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.config.productionTip = false;
+
+// Configuration VueAnalytics
+Vue.use(VueAnalytics, {
+  id: "UA-83611733-3"
+});
 
 new Vue({
   render: h => h(App),
